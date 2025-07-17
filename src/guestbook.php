88ab96d619102a92efc23f6ab2a58d1509518509
Esko Luontola <esko@luontola.fi>
2025-07-18 00:58:49 +0200

    Pagination

--- src/guestbook.php
+++ src/guestbook.php
@@ -4,15 +4,49 @@ require_once "./db.inc";
 require_once "./layout.inc";
 
 print_page_header();
+
+$limit = 10;
+$page = isset($_GET['page']) ? (int)$_GET['page'] : 1;
+$offset = ($page - 1) * $limit;
+
+$result = $conn->query("SELECT COUNT(*) AS total FROM messages");
+$row = $result->fetch_assoc();
+$total_messages = $row['total'];
+$total_pages = ceil($total_messages / $limit);
+
+function print_pagination_links()
+{
+    global $page, $total_pages;
+    echo "<div>";
+    for ($i = 1; $i <= $total_pages; $i++) {
+        if ($i == $page) {
+            echo "<strong>$i</strong> ";
+        } else {
+            echo "<a href='?page=$i#messages'>$i</a> ";
+        }
+    }
+    echo "</div>";
+}
+
 ?>
 
 <h2>Guestbook</h2>
 
-<?php
-$result = $conn->query("SELECT id, content FROM messages ORDER BY id");
-while ($message = $result->fetch_assoc()) {
-    echo "<p>Message " . htmlspecialchars($message['id']) . ": " . htmlspecialchars($message["content"]) . "</p>\n";
-}
+<div id="messages">
+    <?php
+    $stmt = $conn->prepare("SELECT id, content FROM messages ORDER BY id LIMIT ? OFFSET ?");
+    $stmt->bind_param("ii", $limit, $offset);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
+    print_pagination_links();
+    while ($message = $result->fetch_assoc()) {
+        echo "<p>Message " . htmlspecialchars($message['id']) . ": " . htmlspecialchars($message["content"]) . "</p>\n";
+    }
+    print_pagination_links()
+    ?>
+</div>
+
+<?php
 print_page_footer();
 ?>
