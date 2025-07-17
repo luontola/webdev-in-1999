4242f0d66a81bdf8353e121b828b0af874daa348
Esko Luontola <esko@luontola.fi>
2025-07-17 17:20:09 +0200

    Page layout using Simple.css

--- index.php
+++ index.php
@@ -9,31 +9,43 @@
 </head>
 <body>
 
-<?php
-$host = 'db:3306';
-$username = 'db';
-$password = 'db';
-$database = 'db';
+<header>
+    <nav>
+        <a href="/">Home</a>
+    </nav>
+    <h1>LAMP Stack</h1>
+    <p>Web development like it is 1999</p>
+</header>
 
-// https://www.php.net/manual/en/mysqli.construct.php
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$mysqli = new mysqli($host, $username, $password, $database);
-$mysqli->set_charset('utf8mb4');
+<main>
 
-$result = $mysqli->query("SELECT 1+2 as result");
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        echo "<p>Result: " . $row["result"] . "</p>";
+    <?php
+    $host = 'db:3306';
+    $username = 'db';
+    $password = 'db';
+    $database = 'db';
+
+    // https://www.php.net/manual/en/mysqli.construct.php
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $mysqli = new mysqli($host, $username, $password, $database);
+    $mysqli->set_charset('utf8mb4');
+
+    $result = $mysqli->query("SELECT 1+2 as result");
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<p>Result: " . $row["result"] . "</p>";
+        }
+    } else {
+        echo "<p>No results found.</p>";
     }
-} else {
-    echo "<p>No results found.</p>";
-}
 
-$mysqli->close();
+    $mysqli->close();
 
-?>
+    ?>
 
-<p class="hello">Done</p>
+    <p class="hello">Done</p>
+
+</main>
 
 </body>
 </html>
