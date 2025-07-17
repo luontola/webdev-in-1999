ea16774b3d9ec55082a9072cc13c907f29081c48
Esko Luontola <esko@luontola.fi>
2025-07-17 17:35:45 +0200

    Extract layout.inc

--- src/index.php
+++ src/index.php
@@ -1,42 +1,22 @@
 <?php
 global $mysqli;
-include "./db.inc"
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>LAMP - Hello world</title>
-    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.css">
-    <link rel="stylesheet" href="/style.css">
-    <script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.js"></script>
-</head>
-<body>
+include "./db.inc";
+include "./layout.inc";
 
-<header>
-    <nav>
-        <a href="/">Home</a>
-    </nav>
-    <h1>LAMP Stack</h1>
-    <p>Web development like it is 1999</p>
-</header>
+print_page_header();
 
-<main>
-
-    <?php
-    $result = $mysqli->query("SELECT 1+2 as result");
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            echo "<p>Result: " . $row["result"] . "</p>";
-        }
-    } else {
-        echo "<p>No results found.</p>";
+$result = $mysqli->query("SELECT 1+2 as result");
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<p>Result: " . $row["result"] . "</p>";
     }
-    ?>
+} else {
+    echo "<p>No results found.</p>";
+}
+?>
 
-    <p class="hello">Done</p>
+<p class="hello">Hello CSS</p>
 
-</main>
-
-</body>
-</html>
+<?php
+print_page_footer();
+?>
